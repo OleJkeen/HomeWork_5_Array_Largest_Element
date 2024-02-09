7bec84d8628b55d2741276aabc9462b98ffdcf46
OleJkeen <chikenfresh@gmail.com>
2024-02-09 17:35:43 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_5_Array_Largest_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfColumns = 10;
+            int numberOfLines = 10;
+            int maxElement = int.MinValue;
+            int maxLines = 0;
+            int maxColumns = 0;
+            int minRandomNumber = 10;
+            int maxRandomNumber = 99;
+
+            int[,] array = new int[numberOfLines, numberOfColumns];
+            Random randomArrayNumbers = new Random();
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = randomArrayNumbers.Next(minRandomNumber +1, maxRandomNumber +1);
+                    Console.Write(array[i, j] + " ");
+                    
+                    if (maxElement < array[i, j])
+                    {
+                        maxElement = array[i, j];
+                        maxLines = i;
+                        maxColumns = j;
+                        array[maxLines, maxColumns] = 0;
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n-----------------------------\n");
+
+            for (int i =0; i < array.GetLength(0); i++)
+            {
+                for (int j =0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"\nМаксимальное число в массиве {maxElement}");
+            Console.ReadLine();
+        }
+    }
+}
