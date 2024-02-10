ced428f76021f0a0b7feab2ac55f6eb8e308199e
OleJkeen <chikenfresh@gmail.com>
2024-02-10 11:16:50 -0500

    add cycle - count

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ namespace HomeWork_5_Array_Largest_Element
             int maxColumns = 0;
             int minRandomNumber = 10;
             int maxRandomNumber = 99;
+            int count = 0;
 
             int[,] array = new int[numberOfLines, numberOfColumns];
             Random randomArrayNumbers = new Random();
@@ -26,15 +27,19 @@ namespace HomeWork_5_Array_Largest_Element
             {
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
-                    array[i, j] = randomArrayNumbers.Next(minRandomNumber +1, maxRandomNumber +1);
+                    array[i, j] = randomArrayNumbers.Next(minRandomNumber + 1, maxRandomNumber + 1);
                     Console.Write(array[i, j] + " ");
-                    
                     if (maxElement < array[i, j])
                     {
                         maxElement = array[i, j];
                         maxLines = i;
                         maxColumns = j;
                         array[maxLines, maxColumns] = 0;
+                        count = 1;
+                    }
+                    else if (array[i, j] == maxElement)
+                    {
+                        count++;
                     }
                 }
                 Console.WriteLine();
@@ -42,9 +47,25 @@ namespace HomeWork_5_Array_Largest_Element
 
             Console.WriteLine("\n-----------------------------\n");
 
-            for (int i =0; i < array.GetLength(0); i++)
+            for (int i = 0; i < array.GetLength(0); i++)
             {
-                for (int j =0; j < array.GetLength(1); j++)
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] == maxElement)
+                    {
+                        array[i, j] = 0;
+                        count--;
+                    }
+                    else if (count == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
                 {
                     Console.Write(array[i, j] + " ");
                 }
