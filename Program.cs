d04cc030f5432601a988ee80cff7c6466fec46e9
OleJkeen <chikenfresh@gmail.com>
2024-02-10 19:00:50 -0500

    teper tochno completed

--- Program.cs
+++ Program.cs
@@ -14,65 +14,50 @@ namespace HomeWork_5_Array_Largest_Element
             int numberOfColumns = 10;
             int numberOfLines = 10;
             int maxElement = int.MinValue;
-            int maxLines = 0;
-            int maxColumns = 0;
             int minRandomNumber = 10;
             int maxRandomNumber = 99;
-            int count = 0;
 
             int[,] array = new int[numberOfLines, numberOfColumns];
-            Random randomArrayNumbers = new Random();
+            Random random = new Random();
 
             for (int i = 0; i < array.GetLength(0); i++)
             {
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
-                    array[i, j] = randomArrayNumbers.Next(minRandomNumber + 1, maxRandomNumber + 1);
+                    array[i, j] = random.Next(minRandomNumber, maxRandomNumber + 1);
                     Console.Write(array[i, j] + " ");
+                    
                     if (maxElement < array[i, j])
                     {
                         maxElement = array[i, j];
-                        maxLines = i;
-                        maxColumns = j;
-                        array[maxLines, maxColumns] = 0;
-                        count = 1;
                     }
-                    else if (array[i, j] == maxElement)
-                    {
-                        count++;
-                    }
-                }
-                Console.WriteLine();
-            }
-
-            Console.WriteLine("\n-----------------------------\n");
-
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    if (array[i, j] == maxElement)
-                    {
-                        array[i, j] = 0;
-                        count--;
-                    }
-                    else if (count == 0)
-                    {
-                        break;
-                    }
-                }
-            }
-
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    Console.Write(array[i, j] + " ");
                 }
                 Console.WriteLine();
             }
 
             Console.WriteLine($"\nМаксимальное число в массиве {maxElement}");
+            Console.WriteLine("\n\n-----------------------------\n");
+
+            for (int i =0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (maxElement == array[i, j])
+                    {
+                        array[i, j] = 0;
+                    }
+                }
+            }
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
             Console.ReadLine();
         }
     }
